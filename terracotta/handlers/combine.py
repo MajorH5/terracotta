df46a26d740e31814fad9e56196a4ffd0f03e3d0
Habib A. <73318032+MajorH5@users.noreply.github.com>
2025-03-13 03:18:10 +0000

    complete functionality

--- terracotta/handlers/combine.py
+++ terracotta/handlers/combine.py
@@ -3,15 +3,113 @@
 
 Handle /combine API endpoint.
 """
-from typing import BinaryIO
+from typing import Sequence, Tuple, Optional, TypeVar, BinaryIO
+from concurrent.futures import Future
+
+from terracotta import get_driver, get_settings, image, xyz, exceptions
 from terracotta.profile import trace
+
+NumberOrString = TypeVar("NumberOrString", int, float, str)
+ListOfRanges = Sequence[
+    Optional[Tuple[Optional[NumberOrString], Optional[NumberOrString]]]
+]
+
 
 @trace("combine_handler")
 def combine(
-        
+    keys_list: list[list[str]],
+    rgb_values: Optional[Tuple[str, str, str]] = None,
+    tile_xyz: Optional[Tuple[int, int, int]] = None,
+    stretch_ranges: Optional[ListOfRanges] = None,
+    tile_size: Optional[Tuple[int, int]] = None,
 ) -> BinaryIO:
-    """Return image combining all datasets in view
+    """Returns a combined image from multiple datasets based on the provided keys
 
-    Description
+    Band data is retrieved asynchronously for each specified key.
     """
-    pass
+    import numpy as np
+
+    # make sure all stretch ranges contain two values
+    if stretch_ranges is None:
+        stretch_ranges = [None, None, None]
+
+    if len(stretch_ranges) != 3:
+        raise exceptions.InvalidArgumentsError(
+            "stretch_ranges argument must contain 3 values"
+        )
+
+    stretch_ranges_ = [
+        stretch_range or (None, None) for stretch_range in stretch_ranges
+    ]
+
+    if rgb_values is not None:
+        if len(rgb_values) != 3:
+            raise exceptions.InvalidArgumentsError(
+                "rgb_values argument must contain 3 values"
+            )
+
+    settings = get_settings()
+
+    if tile_size is None:
+        tile_size_ = settings.DEFAULT_TILE_SIZE
+    else:
+        tile_size_ = tile_size
+
+    driver = get_driver(settings.DRIVER_PATH, provider=settings.DRIVER_PROVIDER)
+    
+    with driver.connect():
+        key_names = driver.key_names
+
+        for i in range(len(keys_list)):
+            some_keys = keys_list[i]
+            if len(some_keys) != len(key_names) - 1:
+                raise exceptions.InvalidArgumentsError(
+                    f"must specify all keys except last for keys_list[{i}]"
+                )
+
+        def get_band_future(some_keys: list[str], band_key: Optional[str] = None) -> Future:
+            band_keys = (*some_keys, band_key) if band_key else some_keys
+
+            return xyz.get_tile_data(
+                driver,
+                band_keys,
+                tile_xyz=tile_xyz,
+                tile_size=tile_size_,
+                asynchronous=True,
+            )
+
+        if rgb_values is not None:
+            # for each dataset get its red, green, and blue channel data
+            futures = [get_band_future(some_keys, b_key) for some_keys in keys_list for b_key in rgb_values]
+        else:
+            futures = [get_band_future(some_keys) for some_keys in keys_list]
+
+        out_arrays = []
+        band_items = zip(rgb_values, stretch_ranges_, futures)
+
+        for i, (band_key, band_stretch_override, band_data_future) in enumerate(
+            band_items
+        ):
+            keys = (*some_keys, band_key)
+            metadata = driver.get_metadata(keys)
+
+            band_stretch_range = list(metadata["range"])
+            scale_min, scale_max = band_stretch_override
+
+            percentiles = metadata.get("percentiles", [])
+            if scale_min is not None:
+                band_stretch_range[0] = image.get_stretch_scale(scale_min, percentiles)
+
+            if scale_max is not None:
+                band_stretch_range[1] = image.get_stretch_scale(scale_max, percentiles)
+
+            if band_stretch_range[1] < band_stretch_range[0]:
+                raise exceptions.InvalidArgumentsError(
+                    "Upper stretch bound must be higher than lower bound"
+                )
+
+            band_data = band_data_future.result()
+            out_arrays.append(image.to_uint8(band_data, *band_stretch_range))
+    
+    out = np.ma.stack(out_arrays, axis=-1)
+    return image.array_to_png(out)
--- terracotta/server/combine.py
+++ terracotta/server/combine.py
@@ -3,12 +3,13 @@
 Flask route to handle /combine calls.
 """
 
-from typing import Tuple
+from typing import Tuple, Optional
 
 from marshmallow import Schema, fields, validate, ValidationError
 from flask import request, send_file, Response
 
 from terracotta import exceptions
+from terracotta.server.fields import StringOrNumber, validate_stretch_range
 from terracotta.server.flask_api import TILE_API
 
 class CombineQuerySchema(Schema):
@@ -24,9 +25,56 @@ class CombineDatasetsSchema(Schema):
     )
     rgb_keys = fields.List(
         validate=validate.Length(equal=3),
-        example=["R", "G", "B"],
+        example=["red", "green", "blue"],
         required=False, description="Keys for red, green, and blue bands",
         cls_or_instance=fields.String()
+    )
+
+class CombineOptionSchema(Schema):
+    r_range = fields.List(
+        StringOrNumber(allow_none=True, validate=validate_stretch_range),
+        validate=validate.Length(equal=2),
+        example="[0,1]",
+        missing=None,
+        description=(
+            "Stretch range [min, max] to use for the red band as JSON array. "
+            "Min and max may be numbers to use as absolute range, or strings "
+            "of the format `p<digits>` with an integer between 0 and 100 "
+            "to use percentiles of the image instead. "
+            "Null values indicate global minimum / maximum."
+        ),
+    )
+    g_range = fields.List(
+        StringOrNumber(allow_none=True, validate=validate_stretch_range),
+        validate=validate.Length(equal=2),
+        example="[0,1]",
+        missing=None,
+        description=(
+            "Stretch range [min, max] to use for the gren band as JSON array. "
+            "Min and max may be numbers to use as absolute range, or strings "
+            "of the format `p<digits>` with an integer between 0 and 100 "
+            "to use percentiles of the image instead. "
+            "Null values indicate global minimum / maximum."
+        ),
+    )
+    b_range = fields.List(
+        StringOrNumber(allow_none=True, validate=validate_stretch_range),
+        validate=validate.Length(equal=2),
+        example="[0,1]",
+        missing=None,
+        description=(
+            "Stretch range [min, max] to use for the blue band as JSON array. "
+            "Min and max may be numbers to use as absolute range, or strings "
+            "of the format `p<digits>` with an integer between 0 and 100 "
+            "to use percentiles of the image instead. "
+            "Null values indicate global minimum / maximum."
+        ),
+    )
+    tile_size = fields.List(
+        fields.Integer(),
+        validate=validate.Length(equal=2),
+        example="[256,256]",
+        description="Pixel dimensions of the returned PNG image as JSON list.",
     )
 
 @TILE_API.route("/combine/<int:tile_z>/<int:tile_x>/<int:tile_y>.png", methods=["POST"])
@@ -44,6 +92,8 @@ def get_combine(tile_z: int, tile_y: int, tile_x: int) -> Response:
         parameters:
             - in: path
               schema: CombineQuerySchema
+            - in: query
+              schema: CombineOptionSchema
         responses:
             200:
                 description: PNG image of combined tiles from all datasets
@@ -53,32 +103,34 @@ def get_combine(tile_z: int, tile_y: int, tile_x: int) -> Response:
                 description: No dataset found for given key combination
     """
     tile_xyz = (tile_x, tile_y, tile_z)
-
-    try:
-        request_body = CombineDatasetsSchema().load(request.get_json())
-    except ValidationError as err:
-        raise exceptions.InvalidArgumentsError(" ".join(err.messages))
-
-    keys_list = request_body.get("keys_list")
-    keys_list = [[key.strip() for key in keys.split("/") if key.strip()] for keys in keys_list]
-
-    rgb_keys = request_body.get("rgb_keys")
-    rgb_keys = (rgb_keys[0], rgb_keys[1], rgb_keys[2])
-
-    return _get_combine_image(keys_list, rgb_keys, tile_xyz=tile_xyz)
+    return _get_combine_image(tile_xyz)
 
 def _get_combine_image(
-    keys_list: list[list[str]],
-    rgb_keys: Tuple[str, str, str] = None,
-    tile_xyz: Tuple[int, int, int] = None
+    tile_xyz: Tuple[int, int, int]
 ) -> Response:
-    from terracotta.handlers.rgb import rgb
+    from terracotta.handlers.combine import combine
 
-    image = rgb(
-        ["key1", "key2"],
-        ("red", "green", "blue"),
-        stretch_ranges=([0, 1], [0, 1], [0, 1]),
-        tile_xyz=tile_xyz
+    body = CombineDatasetsSchema().load(request.get_json())
+    options = CombineOptionSchema().load(request.args)
+
+    keys_list = body.get("keys_list")
+    keys_list = [[key.strip() for key in keys.split("/") if key.strip()] for keys in keys_list]
+
+    rgb_values = body.get("rgb_keys")
+    tile_size = body.get("tile_size")
+    
+    if rgb_values is not None:
+        rgb_values = (rgb_values[0], rgb_values[1], rgb_values[2])
+
+    stretch_ranges = tuple(options.pop(k) for k in ("r_range", "g_range", "b_range"))
+
+    image = combine(
+        keys_list=keys_list,
+        tile_xyz=tile_xyz,
+        rgb_values=rgb_values,
+        tile_size=tile_size,
+        stretch_ranges=stretch_ranges
     )
 
     return send_file(image, mimetype="image/png")
+#
